c4b238ef2fb6c7dca02d91d31afcd4f317f9ae2b
Josiah Egner <projectzero7@gmail.com>
2017-05-11 16:46:27 +0000

    Add User scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   resources :documents
   root 'documents#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509195155) do
+ActiveRecord::Schema.define(version: 20170511164534) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
